adc50af0784df45b4faf0b06c0fac82147c573ae
Chung Xa <wall85vn@yahoo.com>
2024-04-09 22:42:58 +0700

    Update TextInputJNI.java

--- textinput/src/java/com/defold/textinput/TextInputJNI.java
+++ textinput/src/java/com/defold/textinput/TextInputJNI.java
@@ -367,8 +367,12 @@ public class TextInputJNI {
       layoutMasterParams.gravity = Gravity.TOP | Gravity.LEFT;
       layoutMasterParams.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING;
       if (Build.VERSION.SDK_INT < 30) {
-        layoutMasterParams.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE;
+        layoutMasterParams.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                              | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
       }
       if (Build.VERSION.SDK_INT >= 30) {
         layoutMasterParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS;
